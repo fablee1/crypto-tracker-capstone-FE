dbd34163950c22c141e9ac8c1e2ef15a44f46848
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-09-27 16:18:28 +0100

    cleanup and boilerplate finish

---
+++ src/backend/index.ts
@@ -0,0 +1,30 @@
+import axios from "axios"
+
+const backend = axios.create({
+  baseURL: process.env.REACT_APP_BE_URL_DEV,
+  withCredentials: true,
+})
+
+const refreshAccessToken = async () => {
+  await backend.post("/auth/refreshToken")
+}
+
+backend.interceptors.response.use(
+  (response) => response,
+  async function (error) {
+    const failedRequest = error.config
+    if (error.response.status === 401 && failedRequest.url !== "/auth/refreshToken") {
+      await refreshAccessToken()
+
+      const retryRequest = backend(failedRequest)
+      return retryRequest
+    } else if (failedRequest.url === "/auth/refreshToken") {
+      window.location.href = "/login"
+      return Promise.reject(error)
+    } else {
+      return Promise.reject(error)
+    }
+  }
+)
+
+export default backend
